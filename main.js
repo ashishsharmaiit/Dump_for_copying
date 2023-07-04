d8936f67af9f238a5873c73e4a37812fd940d668
ashishsharmaiit <119526028+ashishsharmaiit@users.noreply.github.com>
2023-07-03 22:38:11 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,98 @@
 var peer = new Peer({
-    host: '34.221.178.214',
-    port: 9000,
-    path: '/'
-  });
-  
-  var connections = {};
-  
-  peer.on('open', function (id) {
-    console.log('My peer ID is: ' + id);
-    document.getElementById('myid').innerText = 'My ID: ' + id;
-  
-    setInterval(function() {
-      var request = new XMLHttpRequest();
-      request.open('GET', 'http://34.221.178.214:3000/peers', true);
-      request.onreadystatechange = function() {
-        if (request.readyState === 4 && request.status === 200) {
-          var peers = JSON.parse(request.responseText);
-          console.log('Connected peers:', peers);
-  
-          peers.forEach(function(peerId) {
-            if (peerId !== id && !connections[peerId]) {
-              var conn = peer.connect(peerId);
-              conn.on('open', function() {
-                connections[conn.peer] = conn;
-                console.log('Connected to peer:', conn.peer);
-  
-                var messageToSend = 'Hello, Peer!';
+  host: '34.221.178.214',
+  port: 9000,
+  path: '/'
+});
+
+var connections = {};
+
+peer.on('open', function (id) {
+  console.log('My peer ID is: ' + id);
+  document.getElementById('myid').innerText = 'My ID: ' + id;
+
+  setInterval(function() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://34.221.178.214:3000/peers', true);
+    request.onreadystatechange = function() {
+      if (request.readyState === 4 && request.status === 200) {
+        var peers = JSON.parse(request.responseText);
+        console.log('Connected peers:', peers);
+
+        peers.forEach(function(peerId) {
+          if (peerId !== id && !connections[peerId]) {
+            var conn = peer.connect(peerId);
+            conn.on('open', function() {
+              connections[conn.peer] = conn;
+              console.log('Connected to peer:', conn.peer);
+              var messageToSend = 'Hello, Peer!';
+              try {
                 conn.send(messageToSend);
-              });
-  
-              conn.on('data', function(data) {
-                console.log('Received', data);
-                const messages = document.getElementById('messages');
-                const message = document.createElement('li');
-                message.textContent = 'Received Message: ' + data;
-                messages.appendChild(message);
-              });
-            }
-          });
+                console.log('Message sent:', messageToSend);
+              } catch (err) {
+                console.error('Error sending message:', err);
+              }
+            });
+
+            conn.on('data', function(data) {
+              console.log('Received', data);
+              const messages = document.getElementById('messages');
+              const message = document.createElement('li');
+              message.textContent = 'Received Message: ' + data;
+              messages.appendChild(message);
+            });
+
+            conn.on('close', function() {
+              console.log('Connection closed');
+            });
+
+            conn.on('error', function(err) {
+              console.error('Connection error:', err);
+            });
+          }
+        });
+      }
+    };
+    request.send();
+  }, 10000);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var sendButton = document.getElementById('sendButton');
+  if(sendButton){
+    sendButton.addEventListener('click', function() {
+      var message = document.getElementById('messageInput').value;
+      console.log('Button clicked, message: ', message);
+      Object.values(connections).forEach(function(conn) {
+        console.log('Connection open status:', conn.open);
+        if(conn.open) {
+          console.log('Sending message to:', conn.peer);
+          try {
+            conn.send(message);
+            console.log('Message sent:', message);
+          } catch (err) {
+            console.error('Error sending message:', err);
+          }
         }
-      };
-      request.send();
-    }, 10000); // Every 10 seconds
-  });
-  
-  document.getElementById('sendButton').addEventListener('click', function() {
-    var message = document.getElementById('messageInput').value;
-    console.log('Sending message:', message); 
-    Object.values(connections).forEach(function(conn) {
-      conn.send(message);
-      console.log('Message sent to peer:', conn.peer);
+      });
     });
+  } else {
+    console.log('Send button element not found');
+  }
+});
+
+peer.on('error', function(err) {
+  console.error('PeerJS Error:', err);
+});
+
+peer.on('connection', (conn) => {
+  conn.on('data', (data) => {
+    console.log('Received:', data);
+    const messages = document.getElementById('messages');
+    const message = document.createElement('li');
+    message.textContent = 'Received Message: ' + data;
+    messages.appendChild(message);
   });
-  
-  peer.on('error', function(err) {
-    console.error('PeerJS Error:', err);
+  conn.on('error', function(err) {
+    console.error('Error in connection:', err);
   });
-  
+});
